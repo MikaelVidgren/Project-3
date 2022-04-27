901ec52f5e8d3ef5c127c2770bcce28c5cee6546
MikaelVidgren <2001546@laurea.fi>
2022-04-27 22:01:49 +0300

    formatted

--- BackEnd/controllers/goalController.js
+++ BackEnd/controllers/goalController.js
@@ -1,67 +1,66 @@
-const asyncHandler = require("express-async-handler")
+const asyncHandler = require("express-async-handler");
 
-const Goal = require ("../models/goalModel")
+const Goal = require("../models/goalModel");
 
 //comment placeholder
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find()
-    res.status(200).json (goals)
-})
+  const goals = await Goal.find();
+  res.status(200).json(goals);
+});
 
 //comment placeholder
 const getGoal = asyncHandler(async (req, res) => {
-    res.status(200).json ({ message: "Get goal " + req.params.id})
-})
+  res.status(200).json({ message: "Get goal " + req.params.id });
+});
 
 //comment placeholder
 const setGoal = asyncHandler(async (req, res) => {
-    //if comment (actually checking if not with !-mark)
-    if (!req.body.text) {
-        res.status(400)
-        throw new Error("Add text")
-    }
+  //if comment (actually checking if not with !-mark)
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Add text");
+  }
 
-    const goal = await Goal.create ({
-        text: req.body.text
-    })
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
 
-    res.status(200).json (goal)
-})
+  res.status(200).json(goal);
+});
 
 //comment placeholder
 const deleteGoal = asyncHandler(async (req, res) => {
-    const goal = await Goal.findById(req.params.id)
+  const goal = await Goal.findById(req.params.id);
 
-    if(!goal) {
-        res.status(400)
-        throw new Error("Goal not found")
-    }
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
 
-    await goal.remove()
+  await goal.remove();
 
-    res.status(200).json ({ id: req.params.id })
-})
+  res.status(200).json({ id: req.params.id });
+});
 
 //comment placeholder
 const updateGoal = asyncHandler(async (req, res) => {
-    const goal = await Goal.findById(req.params.id)
+  const goal = await Goal.findById(req.params.id);
 
-    if(!goal) {
-        res.status(400)
-        throw new Error("Goal not found")
-    }
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
 
-    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-    })
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
 
-
-    res.status(200).json (updatedGoal)
-})
+  res.status(200).json(updatedGoal);
+});
 module.exports = {
-    getGoals,
-    getGoal,
-    setGoal,
-    updateGoal,
-    deleteGoal,
-}
+  getGoals,
+  getGoal,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
